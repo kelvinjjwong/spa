70a30077d6f9867d7e03c36c9ef33f4a5bab127e
kelvinjjwong <kelvinjjwong@outlook.com>
2024-08-27 23:58:44 +0800

    Update build_pod.sh

--- build_pod.sh
+++ build_pod.sh
@@ -220,12 +220,12 @@ if [[ "$GIT_BRANCH" != "$CURRENT_VERSION" ]]; then
     git checkout $CURRENT_VERSION
 fi
 git commit -am "build version $CURRENT_VERSION"
-if [[ $? -eq 0 ]]; then
+#if [[ $? -eq 0 ]]; then
     git push --set-upstream origin $CURRENT_VERSION
     if [[ $? -ne 0 ]]; then
        exit -1
     fi
-fi
+#fi
 
 # POD TESTING
 
